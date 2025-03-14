6668cc94eacef199686fa2d686df9449b663494a
Michał Szafarczyk <92851969+Arch4ngel21@users.noreply.github.com>
2025-03-14 23:42:19 +0100

    Added a skeleton for task 5 with docs

--- task_5/agent.py
+++ task_5/agent.py
@@ -1,15 +1,80 @@
 # Skeleton for Agent class
 
 class Agent:
-
     def get_action(self, obs: dict) -> dict:
-        pass
+        """
+        Main function, which gets called during step() of the environment.
+
+        Observation space:
+            game_map: whole grid of board_size, which already has applied visibility mask on it
+            allied_ships: an array of all currently available ships for the player. The ships are represented as a list:
+                (ship id, position x, y, current health points, firing_cooldown, move_cooldown)
+                - ship id: int [0, 1000]
+                - position x: int [0, 100]
+                - position y: int [0, 100]
+                - health points: int [1, 100]
+                - firing_cooldown: int [0, 10]
+                - move_cooldown: int [0, 3]
+            enemy_ships: same, but for the opposing player ships
+            planets_occupation: for each visible planet, it shows the occupation progress:
+                - planet_x: int [0, 100]
+                - planet_y: int [0, 100]
+                - occupation_progress: int [-1, 100]:
+                    -1: planet is unoccupied
+                    0: planet occupied by the 1st player
+                    100: planet occupied by the 2nd player
+                    Values between indicate an ongoing conflict for the ownership of the planet
+            resources: current resources available for building
+
+        Action space:
+            ships_actions: player can provide an action to be executed by every of his ships.
+                The command looks as follows:
+                - ship_id: int [0, 1000]
+                - action_type: int [0, 1]
+                    0 - move
+                    1 - fire
+                - direction: int [0, 3] - direction of movement or firing
+                    0 - right
+                    1 - down
+                    2 - left
+                    3 - up
+                - speed (not applicable when firing): int [0, 3] - a number of fields to move
+            construction: int [0, 10] - a number of ships to be constructed
+
+        :param obs:
+        :return:
+        """
+
+        return {
+            "ships_actions": [],
+            "construction": 0
+        }
+
 
     def load(self, abs_path: str):
+        """
+        Function for loading all necessary weights for the agent. The abs_path is a path pointing to the directory,
+        where the weights for the agent are stored, so remember to join it to any path while loading.
+
+        :param abs_path:
+        :return:
+        """
         pass
 
     def eval(self):
+        """
+        With this function you should switch the agent to inference mode.
+
+        :return:
+        """
         pass
 
     def to(self, device):
+        """
+        This function allows you to move the agent to a GPU. Please keep that in mind,
+        because it can significantly speed up the computations and let you meet the time requirements.
+
+        :param device:
+        :return:
+        """
         pass
